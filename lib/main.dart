43a654b869bd36ef71f56291cd7fa0388374ee0c
IkemotoNanako <chibencaizi@gmail.com>
2023-02-05 21:37:44 +0900

    ダークモード対応

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: MyHomePage(),
     );
   }
